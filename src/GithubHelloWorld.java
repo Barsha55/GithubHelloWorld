eef5b603d294695864ea5ea7e026623751578156
barsha <barsha.sangraula.2022@students.lhschools.org>
2020-02-13 12:42:46 -0500

    called hello world method again

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -2,12 +2,24 @@ public class GithubHelloWorld {
     public static void main(String[] args) {
     System.out.println("Hello world");
     helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    yourName();
     }
 
     public static void helloWorld(){
         System.out.println("Hello Github World");
     }
-
+    public static void yourName(){
+        System.out.println("this is Barsha Sangraula");
 
 
 
